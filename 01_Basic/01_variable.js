0f9a71172049c7b8720768368f0eacc8445b6be6
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-19 08:06:48 +0000

    Variable

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=789456
+let accountEmail="dasranmamkjj@gamil.com"
+var accountPassword="123345"
+accontCity="Ramachandra Pur"
+let accontState;
+
+//Prefer not to use var because of issuue block scop[e and functional scope//
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accontState])
