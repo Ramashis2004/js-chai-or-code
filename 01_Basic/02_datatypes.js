e3b87908451af7b5a2a2e5bfc1c82b86d49ed585
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-19 12:13:26 +0000

    Datatypes In js

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as a new version
+
+//alert(3+3) //we are usoing nodr js not broswer
+
+console.log(3+3);
+
+console.log(3
+    +
+    3)  //cpde rediability should be high
+    console.log("Hitesh")
+
+    //number=> 2 to power 53
+    //bigint
+    //string=>""
+    //boolean=>true/false
+    //null=>standalone value
+    //undefine=>
+    //symbol=>unique
+
+
+    //object
+
+    console.log(typeof"Hitesh");
+    console.log(typeof null);//object
