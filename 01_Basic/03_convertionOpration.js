0b65f1f8c6d7e7bd9b8a352cf5e4ee77af69b99b
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-28 06:58:46 +0000

    03 .Convertion Js

--- 01_Basic/03_convertionOpration.js
+++ 01_Basic/03_convertionOpration.js
@@ -21,3 +21,29 @@ console.log(booleanIsloggedIn);
 //1=>true,0=>false
 //"  "=>false
 //"ram"=>true
+
+
+//**********Opration ********//
+let Value=30;
+let negValue=-Value
+console.log(negValue);
+
+
+console.log(2+2)
+console.log(2*2)
+console.log(2^2)
+
+let str1 ="ram"
+let str2 =" Hari"
+let str3 =str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")//Here we see fist two digit add ir=t mean 3 and add the string 2 so the output is 32
+console.log("1"+2)
+
+
+let Ram=100
+++Ram;
+console.log(Ram)
