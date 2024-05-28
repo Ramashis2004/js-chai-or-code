61807ecc63e6dcc057c90e8d5852f12cd354f051
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-28 07:38:08 +0000

    04.Comparision

---
+++ 01_Basic/04_comparision.js
@@ -0,0 +1,27 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==2);*/
+
+
+//In java script comparison and equaality are difference//
+
+
+
+console.log("2">1);
+console.log(null >1);
+console.log(null == 0);
+console.log(null >=0);
+
+
+
+/*console.log(undefined ==1);
+console.log(undefined >0);
+console.log(undefined <0);*/
+
+//  ===  //
+//  it check strictly it mean it check the data type  //
+console.log("2"===2);
+
+//The above output is false beacuse it check the deta type so the first
+// data type is string and the second data type is integer//
