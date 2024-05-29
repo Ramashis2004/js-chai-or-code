27d33d2a5f47334cb8bd8c352dd94a91b927d153
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-29 04:03:01 +0000

    05.String.js

---
+++ 01_Basic/05_string.js
@@ -0,0 +1,16 @@
+const name="Ramashis"
+const repoCount=200
+
+//console.log(name+repoCount);
+
+const gameName=new String('rama-hr-com')
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+
+const newString=gameName.substring(0,3)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);          //What you konow about Sting in js all method or any other Search chat cpt//
