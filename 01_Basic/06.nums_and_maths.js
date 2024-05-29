796e86678bad3f29c8ac800a7b2440a3db4ed1f6
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-29 04:39:20 +0000

    Numbar and math .js

---
+++ 01_Basic/06.nums_and_maths.js
@@ -0,0 +1,21 @@
+const score=400
+console.log(score);
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(typeof balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+
+const otherNumber=123.8966
+console.log(otherNumber.toPrecision(3));  //to precison mean our requiremrnt  we put the number so that the method to Precision auotomatically roundup and show the answer
+
+
+const hundreds=100000
+console.log(hundreds.toLocaleString('en-EN'));
+
+
+
+
