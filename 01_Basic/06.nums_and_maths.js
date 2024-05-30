582457a8ef9e57b271b7ada6e620d9b0347558fc
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-30 05:44:29 +0000

    06.Number and math.js

--- 01_Basic/06.nums_and_maths.js
+++ 01_Basic/06.nums_and_maths.js
@@ -1,4 +1,4 @@
-const score=400
+/*const score=400
 console.log(score);
 const balance=new Number(100)
 console.log(balance);
@@ -14,8 +14,138 @@ console.log(otherNumber.toPrecision(3));  //to precison mean our requiremrnt  we
 
 
 const hundreds=100000
-console.log(hundreds.toLocaleString('en-EN'));
+console.log(hundreds.toLocaleString('en-EN'));*/
 
 
 
 
+
+//+++++++++++++++++++++++Math+++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4));  //abs means absolute value it mean if it is -4 then automatically it will be 4
+console.log(Math.round(4.63));//round mean it will be automatically round up to a digit
+console.log(Math.ceil(4.2));//ceil mean aalways upper value it mean always upper
+console.log(Math.floor(4.9));  //floor mean always lower value
+console.log(Math.min(4,87,9,1))
+console.log(Math.random())  //It is more besiicaly use it mea radomly the number are sshow like 0.158344433
+console.log((Math.random()*10)+1)//Here we seen *10 because oe desimal point are pass and add one because it always 1.somethig  
+console.log((Math.floor)(Math.random()*10)+1)//it mean the lower value
+
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
+         //in upper formula we see max-min becaude it craete a range +1 beacuse one point +min mean 10 upper 
+
+
+
+/*    //Here all the fuction of math
+: 
+ƒ abs()
+acos
+: 
+ƒ acos()
+acosh
+: 
+ƒ acosh()
+asin
+: 
+ƒ asin()
+asinh
+: 
+ƒ asinh()
+atan
+: 
+ƒ atan()
+atan2
+: 
+ƒ atan2()
+atanh
+: 
+ƒ atanh()
+cbrt
+: 
+ƒ cbrt()
+ceil
+: 
+ƒ ceil()
+clz32
+: 
+ƒ clz32()
+cos
+: 
+ƒ cos()
+cosh
+: 
+ƒ cosh()
+exp
+: 
+ƒ exp()
+expm1
+: 
+ƒ expm1()
+floor
+: 
+ƒ floor()
+fround
+: 
+ƒ fround()
+hypot
+: 
+ƒ hypot()
+imul
+: 
+ƒ imul()
+log
+: 
+ƒ log()
+log1p
+: 
+ƒ log1p()
+log2
+: 
+ƒ log2()
+log10
+: 
+ƒ log10()
+max
+: 
+ƒ max()
+min
+: 
+ƒ min()
+pow
+: 
+ƒ pow()
+random
+: 
+ƒ random()
+round
+: 
+ƒ round()
+sign
+: 
+ƒ sign()
+sin
+: 
+ƒ sin()
+sinh
+: 
+ƒ sinh()
+sqrt
+: 
+ƒ sqrt()
+tan
+: 
+ƒ tan()
+tanh
+: 
+ƒ tanh()
+trunc
+: 
+ƒ trunc()
+
+
+*/
