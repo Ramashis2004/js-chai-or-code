5f8ab41d5675420842a1b50ffe558ac35e64562f
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-05-30 06:38:34 +0000

    07.Datein .js

---
+++ 01_Basic/07.datesin.js
@@ -0,0 +1,10 @@
+//Date
+
+/*let myDate=new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)//So always remember date is an object*/
+ let myCreatedDate = new Date (2023 ,0,23)   //Here we seen our inpuit is 0 but outpt are janauary so 0 means january
+ console.log(myCreatedDate)
