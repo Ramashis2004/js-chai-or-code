2f3cf894d0c685eb55a3141ae0c7e9fb0920e99c
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-06-06 20:42:59 +0000

    2.Do while.js

---
+++ 05_iterationloop/twowhile.js
@@ -0,0 +1,21 @@
+let index=0
+while(index <=10){
+    console.log(`the value is ${index}`);
+    index=index+2
+}
+
+let myArray=["ram","hari","gop"]
+let arr=0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+    
+}
+
+let score=11  //Here first we check
+
+do{
+    console.log(`score is ${score}`);
+    score++
+}
+while(score <=10);
