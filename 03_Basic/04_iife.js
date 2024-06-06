6f7b9a49c223695bdd0099280f367117d23bfd07
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-06-06 14:38:59 +0000

    04_IIFE.js

---
+++ 03_Basic/04_iife.js
@@ -0,0 +1,24 @@
+//Immediately invoke the funcation Expression  (IIFE)
+
+(function chai(){
+    console.log(`db connected`);
+})();
+
+( () =>{
+    console.log(`Db connected two`);           //why we use IIFE
+                                                //Avoid Polluting the Global Namespace:
+
+                                                 //In JavaScript, variables declared within a
+                                                //function are scoped to that function.
+                                                // By using an IIFE, you can create variables
+                                                // and functions without polluting the global scope. This helps prevent potential conflicts with other scripts or libraries.
+}
+
+)()
+;
+( (name) =>{
+    console.log(`Db connected two  ${name}`);
+}
+
+)("rama")
+
