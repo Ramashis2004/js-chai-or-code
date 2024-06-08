cd4d27d79be9dda7372751e60ebc6673e86dbc9d
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-06-08 19:58:32 +0000

    4.forin.js

---
+++ 05_iterationloop/fourforin.js
@@ -0,0 +1,36 @@
+//+++++++++++For IN+++++++++++++
+
+
+
+const myObject={
+
+
+
+
+    js:"Javascript",
+    cpp:"C++",
+    rb:"ruby",
+    swift:"apple"
+}
+    for (const key in myObject) {
+        console.log(`${key} shortcort for symbol ${myObject[key]}`);
+        
+    }
+
+    const programming=["js","java","cpp"]
+    for (const key in programming) {
+       console.log(programming[key]);
+            
+        }
+    
+const map=new Map()
+map.set('IN' ,"India")
+map.set('USA',"Unites sate of america")
+map.set('Fr',"France")
+
+for (const key in map) {
+    console.log(key);
+   
+}
+    
+    
