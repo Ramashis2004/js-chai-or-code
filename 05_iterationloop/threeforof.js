d75c0f6913ac1406d1347f15af775bd88e70a17e
Ramashis2004 <137889024+Ramashis2004@users.noreply.github.com>
2024-06-08 19:58:01 +0000

    3.foreach

--- 05_iterationloop/threeforof.js
+++ 05_iterationloop/threeforof.js
@@ -11,6 +11,21 @@ for (const num of arr) {
 
 const greetings="Hello world"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+    //console.log(`Each char is ${greet}`);
     
 }
+
+//Maps
+
+const map=new Map()
+map.set('IN' ,"India")
+map.set('USA',"Unites sate of america")
+map.set('Fr',"France")
+//map.set('IN' ,"India")   Her we see the code is not run so the map funcation only single time run 
+//console.log(map);
+
+for (const [key,value] of map) {
+    console.log([key,':-', value]);
+    
+}
+
